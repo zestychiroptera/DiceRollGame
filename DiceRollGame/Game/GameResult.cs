4162f6014da4b4519f1fa8892d1daaad3bbdfeaf
Zach Waller <zachjives@gmail.com>
2024-06-28 19:06:45 -0400

    Add project files.

---
+++ DiceRollGame/Game/GameResult.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace DiceRollGame.Game
+{
+    public enum GameResult
+    {
+        Victory,
+        Loss
+    }
+}
+
---
+++ DiceRollGame/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+// When the application starts, it shall generate a number from 1 to 6 and then print: 
+
+// Dice rolled. Guess what number it shows in 3 tries.
+
+// The user must select a number. If the number equals the generated number, “You win” is printed to the console. If not, “Wrong number” is printed, and the user has another chance. The user has 3 chances. If they all are unsuccessful, “You lose” is printed to the console, and the program closes after any key is pressed. 
+
+using DiceRollGame;
+using DiceRollGame.Game;
+using System.Threading.Channels;
+
+var random = new Random();
+var dice = new Dice(random);
+var guessingGame = new GuessingGame(dice);
+
+GameResult gameResult = guessingGame.Play();
+GuessingGame.PrintResult(gameResult);
+
+
+
+Console.ReadKey();
+
+
+
